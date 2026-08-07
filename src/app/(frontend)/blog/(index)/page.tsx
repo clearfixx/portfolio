c04af79838a0b5339f2b4b3fadc259c00c2335b5
Андрій Кулагін <akulagin88@gmail.com>
2026-08-07 12:32:11 +0300

    refactor(blog): isolate rail series styles

--- src/app/(frontend)/blog/(index)/page.tsx
+++ src/app/(frontend)/blog/(index)/page.tsx
@@ -24,6 +24,7 @@ import featuredStyles from './BlogFeaturedArticle.module.scss'
 import toolbarStyles from './BlogArticleToolbar.module.scss'
 import railStyles from './BlogRail.module.scss'
 
+import railSeriesStyles from './BlogRailSeries.module.scss'
 import railTransmissionsStyles from './BlogRailTransmissions.module.scss'
 import paginationShellStyles from './BlogPaginationShell.module.scss'
 import paginationControlsStyles from './BlogPaginationControls.module.scss'
@@ -678,7 +679,7 @@ export default async function BlogPage({ searchParams }: BlogPageProps) {
                   <JournalIcon name="book" size={14} />
                   Series
                 </h2>
-                <ul className={railStyles.seriesList}>
+                <ul className={railSeriesStyles.seriesList}>
                   {series.map((item) => (
                     <li key={item.slug}>
                       <Link href={withQuery(queryState, { page: undefined, series: item.slug })}>
